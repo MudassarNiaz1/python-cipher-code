49816cad5d6de860eb5f1c4befad6839b4ee5c54
Mudassar Niaz <159882845+MudassarNiaz1@users.noreply.github.com>
2024-10-30 16:35:47 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
-from functions import decoder, encoder
+from functions import Cipher
 
-alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-while True:
-  print("option 1")
-  print("option 2")
-  option = int(input("Choose the option"))
+def main():
+    alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-  if option == 1:
-    encode = encoder(alpha)
-    print(f"encode word is {encode}")
-    continue
-  elif option == 2:
-    decode = decoder(alpha)
-    print(f"decode word is {decode}")
-    break
+    object = Cipher(alpha)
+
+    while True:
+        option = int(input('option (1/2): '))
+
+        if option == 1:
+            encode = object.encoder()
+            print(f'Encode: {encode}')
+            continue
+        elif option == 2:
+            decode = object.decoder()
+            print(f'Decode: {decode}')
+            break
+
+if __name__ == '__main__':
+    main()
+
